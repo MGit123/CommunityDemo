5324396cc28ab230372354b8def2efc02b5ca502
unknown <15297879246@163.com>
2019-08-21 19:38:31 +0800

    init repository

---
+++ src/main/java/com/lgx/community/Controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.lgx.community.Controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author admin
+ * @date 2019/8/21 19:02
+ */
+
+@Controller
+public class BaseController {
+
+
+
+}
